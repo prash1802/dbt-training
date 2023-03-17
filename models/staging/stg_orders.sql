243ebbfaff96d4c43cae70ff7e0f7cb8d4569652
gk7112602@gmail.com <gk7112602@gmail.com>
2023-03-17 06:00:29 +0000

    Developing stg_orders1

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,16 +1,17 @@
 select 
 --from raw orders
 o.user_id,
+o.id,
 o.order_date,
 o.status,
-o.id,
 o.user_id - o.id as diffid,
 --from raw customer
 c.first_name,
 c.last_name,
 --from raw payment
 p.paymentmethod,
-p.amount
+p.amount,
+p.orderid
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.user_id=c.id
