52d047a6f72224e91c17c3cddefc544d23d4ccdc
gk7112602@gmail.com <gk7112602@gmail.com>
2023-03-20 13:08:57 +0000

    added status_team seed and added it to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select 
 --from raw orders
+{{ dbt_utils.generate_surrogate_key(['o.id', 'o.user_id','p.orderid']) }} as sk_orders,
 o.user_id,
 o.id,
 o.order_date,
@@ -12,11 +13,13 @@ c.last_name,
 p.paymentmethod,
 p.amount,
 p.orderid,
-{{ markup('orderid','user_id') }} as markup
+{{ markup('orderid','user_id') }} as markup,
+d.status_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.user_id=c.id
 left join {{ ref('raw_payment') }} as p 
 on o.id=p.orderid
-
+left join {{ ref('status_team') }} as d
+on o.status=d.status
 {{ limit_data_in_dev('order_date') }}
